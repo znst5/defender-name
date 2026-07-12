c9fdf84327d523ea2deefd798bf8b11ea0786a68
Anastasia Zorina <znst35@gmail.com>
2026-07-12 21:44:36 +0300

    refactor: модули

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
-console.log('worked');
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js';
+console.log('app worked');
+
+const game = new Game();
+new GameSavingData();
+game.start();
+
+loadGame();
+saveGame();
+
